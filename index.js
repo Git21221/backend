bb412ea1e380c14d4acb8c9c82e8d2cabb4abc0e
Saikat Das <saikatdas40g@gmail.com>
2023-12-18 10:45:21 +0530

    added api

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import express from "express";
 import "dotenv/config";
 const app = express();
+const jokedata = [
+  {
+    "id": 1,
+    "joke": "I am good boy",
+    "Author": "Saikat Das",
+  },
+  {
+    "id":2,
+    "joke": "Sumon is good boy",
+    "Author": "Sumon Mitra",
+  },
+  {
+    "id": 3,
+    "joke": "Sunny is good boy",
+    "Author": "Sunny Mishra",
+  }
+]
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello</h2>");
@@ -13,6 +30,10 @@ app.get("/hi", (req, res) => {
 app.get("/hii", (req, res) => {
   res.send("<h1>dekho firse hii likh rha hai....pdhai kr bhai</h1>");
 });
+
+app.get('/jokes', (req, res) => {
+  res.json(jokedata);
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`server is live at ${process.env.PORT}`);
