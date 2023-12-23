57f21ae98d7435405de596a821b3eaea242f48d0
Saikat Das <saikatdas40g@gmail.com>
2023-12-23 22:27:33 +0530

    added cloudinary and multer (setup done for backend)

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+cloudinary.v2.uploader.upload(
+  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) {
+    console.log(result);
+  }
+);
+
+const uploadFileOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto", 
+    });
+    //file has been uplaoded on cloudinary
+    console.log("file uplaoded", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the locally saved temporary file after uploading got failed
+    return null;
+  }
+};
+
+export { uploadFileOnCloudinary };
